f5bcdd470c1145000cb43f6007e6d4e812f3bf7e
ReicaD <dbamanya447@gmail.com>
2025-01-18 17:46:24 -0500

    added javascript typeOf

---
+++ main.js
@@ -0,0 +1,19 @@
+console.log("HELLO EARTH");
+
+//strings numbers & boolean
+const PersonName = "Jeje";
+const age = 24;
+const rating = 4.5;
+const isCool = true;
+const x = null;
+const y = undefined;
+let z;
+
+// console.log(typeof isCool);
+
+if (Boolean(isCool) === "true") {
+  console.log("isCool is truly cool!");
+} else {
+  console.log("isCool is not truly cool!");
+}
+// console.log(Boolean(isCool));
