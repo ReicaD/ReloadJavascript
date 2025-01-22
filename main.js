46b41b61365108a08925579470514d5c6f410aa0
ReicaD <dbamanya447@gmail.com>
2025-01-22 12:50:25 -0500

    added concatenation Prac

--- main.js
+++ main.js
@@ -1,19 +1,30 @@
-console.log("HELLO EARTH");
+// console.log("HELLO EARTH");
 
-//strings numbers & boolean
-const PersonName = "Jeje";
-const age = 24;
-const rating = 4.5;
-const isCool = true;
-const x = null;
-const y = undefined;
-let z;
+// //strings numbers & boolean
+// const PersonName = "Jeje";
+// const age = 24;
+// const rating = 4.5;
+// const isCool = true;
+// const x = null;
+// const y = undefined;
+// let z;
 
 // console.log(typeof isCool);
 
-if (Boolean(isCool) === "true") {
-  console.log("isCool is truly cool!");
-} else {
-  console.log("isCool is not truly cool!");
-}
-// console.log(Boolean(isCool));
+// if (Boolean(isCool) === "true") {
+//   console.log("isCool is truly cool!");
+// } else {
+//   console.log("isCool is not truly cool!");
+// }
+// // console.log(Boolean(isCool));
+
+// Template literal (concatenation)
+
+const firstName = "Eddy"
+const lastName = "Menzi"
+const age = 23
+
+const introduction = `Hello, My Name is ${firstName} ${lastName}, and i am ${age} young.`
+
+console.log(introduction)
+
